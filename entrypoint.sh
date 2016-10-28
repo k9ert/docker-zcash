664c6733727e5b92db0f1ffaacac859fe84abcaf
Lars-Magnus Skog <ralphtheninja@riseup.net>
2016-10-28 19:14:59 +0200

    forgot to update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,12 @@ set -e
 ZCASH_HOME="/root/.zcash"
 ZCASH_CONF="${ZCASH_HOME}/zcash.conf"
 
-if [ ! -f "${ZCASH_CONF}" ]; then
-    echo "generate ${ZCASH_CONF}"
-    password=$(head /dev/urandom | sha256sum | cut -c1-60)
-    printf '%s\n%s\n%s\n%s\n%s\n' "rpcuser=zcashrpc" "rpcpassword=${password}" \
-           "testnet=1" "addnode=alphatestnet.z.cash" "gen=1" > ${ZCASH_CONF}
-fi
+mkdir -p ${ZCASH_HOME}
 
-# why not use exec here?
+echo "generate ${ZCASH_CONF}"
+echo "addnode=mainnet.z.cash" > ${ZCASH_CONF}
+echo "rpcuser=username" >> ${ZCASH_CONF}
+echo "rpcpassword=$(head -c 32 /dev/urandom | base64)" >> ${ZCASH_CONF}
+
 /usr/local/bin/zcashd
 exit $?
